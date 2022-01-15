1cf540df3bbbe4d5045cfe85024a59fa532ac1e5
SineshX <48027382+SineshX@users.noreply.github.com>
2022-01-15 21:55:09 +0530

    attempt 3

--- Coursera_ASS2.js
+++ Coursera_ASS2.js
@@ -1,5 +1,5 @@
 // visit https://www.dukelearntoprogram.com/course1/example/index.php to execute this code :) 
-// 1 
+// 1 correct
 var image = new SimpleImage("chapel.png");
 
 for (var pixel of image.values()) {
@@ -9,7 +9,7 @@ for (var pixel of image.values()) {
 print(image);
 
 
-// 2
+// 2 correct
 var image = new SimpleImage("chapel.png");
 
 // missing code
@@ -18,7 +18,7 @@ for (var pixel of image.values()) {
 }
 print(image);
 
-// 3
+// 3 correct
 var image = new SimpleImage("eastereggs.jpg");
 
 // missing code
@@ -31,10 +31,10 @@ for (var pixel of someImage.values()) {
 print(image);
 
 
-// 4
+// 4 correct 
 var image = new SimpleImage("astrachan.jpg");
 
-// missing code
+// missing code 1
 
 var height = someImage.getHeight();
 for (var pixel of someImage.values()) {
@@ -46,16 +46,19 @@ for (var pixel of someImage.values()) {
 }
 print(image);
 
-// 5
+// 5 in correct (not)
 var someImage = new SimpleImage("chapel.png");
 
 // missing code
-
 for (var pixel of someImage.values()) {
+    if (pixel.getX() < 50) {
+        if (pixel.getY() < 50) {
+            pixel.setGreen(255);
+        }
+    }
     if (pixel.getY() < 50) {
         if (pixel.getX() < 50) {
             pixel.setRed(0);
-            pixel.setGreen(255);
             pixel.setBlue(0);
         }
     }
@@ -64,11 +67,12 @@ print(someImage);
 
 
 
-// 6
+// 6 correct 
 function topRightCorner(cornerWidth, cornerHeight, someImage, red, green, blue) {
     var width = someImage.getWidth();
     for (var pixel of someImage.values()) {
         // missing code
+        
        if (pixel.getY() < cornerHeight) {
             if (pixel.getX() > width - cornerWidth) {
                 pixel.setRed(red);
@@ -84,7 +88,7 @@ function topRightCorner(cornerWidth, cornerHeight, someImage, red, green, blue) 
 }
 
 
-// 7
+// 7 correct 
 
 function topRightCorner(cornerWidth, cornerHeight, someImage, red, green, blue) {
     var width = someImage.getWidth();
@@ -110,27 +114,28 @@ print(result3);
 
 
 
-// Q8
+// Q8 in correct
 function changeRed(width, height) {
     var picture = new SimpleImage(width, height);
     var red = 0;
 
-     // missing code
-     for (var pixel of picture.values()) {
+     // missing code 2
+    for (var pixel of picture.values()) {
         pixel.setRed(red);
         pixel.setGreen(0);
         pixel.setBlue(0);
+        if (pixel.getRed() < 255) {
+            red = red + 1;
+        }
         if (pixel.getRed() == 255) {
             red = 0;
         }
-        if (pixel.getRed() != 255) {
-            red = red + 1;
-        }
-    }
-
+}
     return picture;
 }
 
 var result = changeRed(256,200);
 print(result);
 
+// 9 correct :)
+// Add two parameters, one for the blue color and one for the green color. Then in the code set blue to be the blue number passed in and green to be the green number passed in. 
